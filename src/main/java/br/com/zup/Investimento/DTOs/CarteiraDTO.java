3a7f62557744872f178f434509c36946e5bdaceb
Savio <pauloluizsavio@gmail.com>
2022-02-18 11:10:56 -0300

    Criar carteira dto

---
+++ src/main/java/br/com/zup/Investimento/DTOs/CarteiraDTO.java
@@ -0,0 +1,12 @@
+package br.com.zup.Investimento.DTOs;
+
+import lombok.Data;
+
+@Data
+public class CarteiraDTO {
+
+    private double valorInvestido;
+    private double valorTotalLucro;
+    private double valorTotal;
+
+}
