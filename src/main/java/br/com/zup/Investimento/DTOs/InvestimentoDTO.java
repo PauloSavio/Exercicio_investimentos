b3109cc05f65050abfdaf6fa481c9ef7427e3cfa
Savio <pauloluizsavio@gmail.com>
2022-02-18 11:17:44 -0300

    Criar investimento dto

---
+++ src/main/java/br/com/zup/Investimento/DTOs/InvestimentoDTO.java
@@ -0,0 +1,70 @@
+package br.com.zup.Investimento.DTOs;
+
+import br.com.zup.Investimento.Enums.Risco;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+
+public class InvestimentoDTO {
+    @Email
+    private String email;
+    private String nome;
+    @CPF(message = "CPF invalido")
+    private String cpf;
+    private double valorInvestido;
+    @Min(2)
+    private int periodoDeAplicacaoMeses;
+    private Risco risco;
+
+    public InvestimentoDTO() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getValorInvestido() {
+        return valorInvestido;
+    }
+
+    public void setValorInvestido(double valorInvestido) {
+        this.valorInvestido = valorInvestido;
+    }
+
+    public int getPeriodoDeAplicacaoMeses() {
+        return periodoDeAplicacaoMeses;
+    }
+
+    public void setPeriodoDeAplicacaoMeses(int periodoDeAplicacaoMeses) {
+        this.periodoDeAplicacaoMeses = periodoDeAplicacaoMeses;
+    }
+
+    public Risco getRisco() {
+        return risco;
+    }
+
+    public void setRisco(Risco risco) {
+        this.risco = risco;
+    }
+}
